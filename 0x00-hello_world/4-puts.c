bf1d030bd78274fa01478dd5dfb3c146069660b8
Lohkrii <joshualopez0429@gmail.com>
2020-09-16 19:29:35 +0000

    Added File 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+
+/**
+* main - Prints "Programming is like building a multilingual puzzle"
+*
+* Return: Returns inputted string
+*/
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle");
 	getchar();
 	return (0);
 }
