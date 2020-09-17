db1ad78a87cac8937e5ef5c2b7572dff298bf8a1
Lohkrii <joshualopez0429@gmail.com>
2020-09-17 03:27:48 +0000

    Added File 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the values of all the types on the computer.
+ *
+ * Return: Must be 0 to work
+*/
+
+int main(void)
+{
+	printf(NNN, xHH, uHHH, UHHHHHHHH);
+	return (0);
+}
