9ab6457d5976f0afeca234510cc7849fa4379183
Lohkrii <joshualopez0429@gmail.com>
2020-09-18 15:37:30 +0000

    Added File: 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase letters
+ *
+ * Return: Must return 0 for success
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	putchar(x);
+	}
+	return (0);
+}
