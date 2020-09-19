716819d5b6fe83fb14be9a8ab0ab26833a59b513
Lohkrii <joshualopez0429@gmail.com>
2020-09-19 03:44:28 +0000

    Update File 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -10,12 +10,15 @@ int main(void)
 {
 	int x = '0';
 
-	for (x = 0; x <= 100; x++)
+	for (x = '0'; x <= 99; x++)
 	{
 	putchar((x / 10) + '0');
 	putchar((x % 10) + '0');
+	if (x != 99)
+	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
