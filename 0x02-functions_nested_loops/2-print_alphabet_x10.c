d74af9bdcd2d9b7bacb9cbfffee175146a806e1b
Lohkrii <joshualopez0429@gmail.com>
2020-09-23 14:53:27 +0000

    Uodate

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,11 @@
  *  print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
  *
 */
-
 void print_alphabet_x10(void)
 {
 	int x;
 
-	for (x = 0; x < 9; x++)
+	for (x = 0; x < 10; x++)
 	{
 		char n;
 
