e83b7c933cde9f2d82d5137cefde4d99162ffe94
Lohkrii <joshualopez0429@gmail.com>
2020-09-23 04:14:30 +0000

    Added Files: holberton.h, 0-holberton.c & _putchar.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - Prints to st "Holberton"
+ *
+ * Return: Must return 0 for compliance.
+*/
+
+int main(void)
+{
+	char n[9] = "Holberton";
+	int x;
+
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(n[x]);
+	}
+	return (0);
+}
