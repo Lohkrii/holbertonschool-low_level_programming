d3f6b094f51736168fcf704aa3fa9050feaff5d8
Lohkrii <joshualopez0429@gmail.com>
2020-09-24 03:04:14 +0000

    Update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _abs - Prints absolute value of integer.
+ *
+ * @int: is the input variable.
+ *
+ * Return: must be 0
+*/
+
+int _abs(int)
+{
+	int N;
+
+	if (N < 0)
+	{
+		N = (-1) * N;
+	}
+	_putchar('\n');
+	return (0);
+}
