0f18ba26e36d550f425161fce3e9e4fc25e6fa4c
Khalah Jones-Golden <khasan222@gmail.com>
2020-10-09 19:49:59 +0000

    Added Requirements and task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+*/
+
+int main(int argc, char **argv)
+{
+	int idx;
+
+	for (idx = 0; idx < argc; idx++)
+	{
+		printf("%s\n", argv[idx]);
+	}
+		return (0);
+}
