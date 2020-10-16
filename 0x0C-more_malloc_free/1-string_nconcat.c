8d055ea82d79cc58de230776d58fa3ecb1870372
Lohkrii <joshualopez0429@gmail.com>
2020-10-16 19:04:19 +0000

    Task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Writes function that concatonates 2 strings.
+ * @s1: Contains the first string.
+ * @s2: Contains the second string.
+ * @n: Contains number of bytes for excersize complication.
+ * Return: Returns the pointer to the adress that holds\
+ * the concatonated string.
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int idx;
+	unsigned int jdx;
+	char *ptr;
+	unsigned int s1_length = 0;
+	unsigned int s2_length = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (idx = 0; s1[idx] != '\0'; idx++)
+	{
+		s1_length++;
+	}
+	for (idx = 0; s2[idx] <= '\0'; idx++)
+	{
+		s2_length++;
+	}
+	if (n >= s2_length)
+	{
+		n = s2_length;
+	}
+	ptr = malloc(s1_length + n);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (idx = 0; *(s1 + idx) != '\0'; idx++)
+	{
+		ptr[idx] = s1[idx];
+	}
+	for (jdx = 0; jdx <= n; jdx++)
+	{
+		ptr[idx + jdx] = s2[jdx];
+	}
+	return (ptr);
+}
