68b8ac586cfad440b5928be07e1d4172046bc32e
Lohkrii <joshualopez0429@gmail.com>
2020-10-22 18:31:40 +0000

    Update 1

--- 0x0F-funtion_pointers/0-print_name.c
+++ 0x0F-funtion_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 /**
  * print_name - Prints a name.
  * @name: Allocates the input name in array.
- * @f: Calls upon a function 
+ * @f: Calls upon a function.
 */
 
 void print_name(char *name, void (*f)(char *))
