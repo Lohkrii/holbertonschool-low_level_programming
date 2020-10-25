1392dda6c819e2b5de2d66bab800834fdd7ceb2a
Lohkrii <joshualopez0429@gmail.com>
2020-10-25 23:06:23 +0000

    Update putchar

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,4 +2,7 @@
 #define variadic_functions
 /** Sums the passed arguments**/
 int sum_them_all(const unsigned int n, ...);
+
+/** inputs a singular character **/
+int _putchar(char x);
 #endif /** variadic_functions **/
