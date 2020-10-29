378d3d0f0199df942047f715defd1ebd9cc73859
Lohkrii <joshualopez0429@gmail.com>
2020-10-29 18:38:20 +0000

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,6 +4,7 @@
  * add_node - Adds a node at the beginning of a list.
  * @head: is the pointer to the current head node.
  * @str: Is the data held in the node.
+ * Return: Returns the adress of the new node or returns NULL.
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -25,7 +26,8 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	else
 	{
-		for (idx = 0; *(str + idx) != '\0'; idx++);
+		for (idx = 0; *(str + idx) != '\0'; idx++)
+			;
 		new->str = strdup(str);
 		new->len = idx;
 	}
