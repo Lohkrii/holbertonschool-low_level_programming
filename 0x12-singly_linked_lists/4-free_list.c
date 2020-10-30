bb92a63a4315712f03e8286edc479a93a2b48d22
Lohkrii <joshualopez0429@gmail.com>
2020-10-30 20:07:15 +0000

    Update task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,9 +10,14 @@ void free_list(list_t *head)
 	list_t *node, *temp;
 
 	node = head;
+	if (node == NULL)
+	{
+		return;
+	}
 	while (node != NULL)
 	{
 		temp = node->next;
+		free(node->str);
 		free(node);
 		node = temp;
 	}
