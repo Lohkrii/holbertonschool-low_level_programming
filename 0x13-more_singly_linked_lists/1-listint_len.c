e2ecb9ec3dd39bbbb7ad43818cb5d0147ea1c2b8
Lohkrii <joshualopez0429@gmail.com>
2020-11-02 18:41:48 +0000

    Update task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -18,6 +18,10 @@ size_t listint_len(const listint_t *h)
 		printf("Error\n");
 		return (0);
 	}
+	if (h == NULL)
+	{
+		return (0);
+	}
 	temp = h->next;
 	for (ecount = 1; temp->n != '\0'; ecount++)
 	{
