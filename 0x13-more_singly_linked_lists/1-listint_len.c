7f02549885b73cc347fe29c66d224af866d94eb3
Lohkrii <joshualopez0429@gmail.com>
2020-11-02 18:50:47 +0000

    Update task 0

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,40 @@
 #include "lists.h"
 
 /**
- * listint_len - calculates the amount of elements in a linked list
- * @h: head of the list
- * Return: amount of nodes
+ * listint_len - Return the number of elements in a linked list.
+ * @h: Is the head pointer.
+ * Return: Returns the number of elements.
  */
+
 size_t listint_len(const listint_t *h)
 {
-	size_t len = 0;
+	const listint_t *temp;
+	unsigned int ecount;
 
-	while (h != NULL)
+	temp = malloc(sizeof(listint_t));
+
+	if (temp == NULL)
 	{
-		len++;
-		h = h->next;
+		printf("Error\n");
+		return (0);
 	}
-
-	return (len);
+	if (h == NULL)
+	{
+		return (0);
+	}
+	if (h->n == '\0')
+	{
+		return (0);
+	}
+	temp = h->next;
+	for (ecount = 1; temp->n != '\0'; ecount++)
+	{
+		if (temp->next == NULL)
+		{
+			ecount++;
+			break;
+		}
+		temp = temp->next;
+	}
+	return (ecount);
 }
