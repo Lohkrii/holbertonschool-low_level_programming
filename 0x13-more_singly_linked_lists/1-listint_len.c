96ca475a528d0d11ba8ea64e7e4502ed63115dad
Lohkrii <joshualopez0429@gmail.com>
2020-11-02 18:53:30 +0000

    Update task 0

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,33 +8,11 @@
 
 size_t listint_len(const listint_t *h)
 {
-	const listint_t *temp;
 	unsigned int ecount;
 
-	temp = malloc(sizeof(listint_t));
-
-	if (temp == NULL)
+	for (ecount = 0; h != NULL; ecount++)
 	{
-		printf("Error\n");
-		return (0);
-	}
-	if (h == NULL)
-	{
-		return (0);
-	}
-	if (h->n == '\0')
-	{
-		return (0);
-	}
-	temp = h->next;
-	for (ecount = 1; temp->n != '\0'; ecount++)
-	{
-		if (temp->next == NULL)
-		{
-			ecount++;
-			break;
-		}
-		temp = temp->next;
+		h = h->next;
 	}
 	return (ecount);
 }
