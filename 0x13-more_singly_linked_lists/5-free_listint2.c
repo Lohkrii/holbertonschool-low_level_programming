08758187361747d4b5716951e877dbd6901af43b
Lohkrii <joshualopez0429@gmail.com>
2020-11-03 22:42:20 +0000

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,9 +9,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp, *crnt_node;
 
-	temp = *head;
 	if (head != NULL)
 	{
+		*temp = head;
 		while (temp != NULL)
 		{
 			crnt_node = temp->next;
