0885e2c60e4acc365d14b08b0828565754e0cc76
Lohkrii <joshualopez0429@gmail.com>
2020-11-04 00:46:14 +0000

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,23 +12,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int count;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-	temp = head;
+	*temp = head;
 	while (temp != NULL)
 	{
 		if (count == index)
 		{
 			return (temp);
 		}
-		temp = temp->next;
 		count++;
+		temp = temp->next;
 	}
-	if (temp == NULL)
-	{
-		return (NULL);
-	}
-	return (temp);
+	return (NULL);
 }
