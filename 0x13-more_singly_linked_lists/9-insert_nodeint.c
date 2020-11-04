846761c656e01c81a30b541308edc18ab3d6e267
Lohkrii <joshualopez0429@gmail.com>
2020-11-04 01:37:51 +0000

    Update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		temp = temp->next;
 	}
-	if (idx_run == idx -1)
+	if (idx_run == idx - 1)
 	{
 		new = temp->next;
 		new->n = n;
