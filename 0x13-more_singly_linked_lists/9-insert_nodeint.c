7345f710904000f986718a379049460a31bbb4e7
Lohkrii <joshualopez0429@gmail.com>
2020-11-04 01:44:08 +0000

    Update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	if (idx == '\0')
+	{
+		return (NULL);
+	}
 	if (head != NULL)
 	{
 		temp = *head;
