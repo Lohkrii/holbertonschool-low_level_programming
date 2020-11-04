22c3708cc7497e20fadef7d6dc3207185a1f7ba5
Lohkrii <joshualopez0429@gmail.com>
2020-11-04 01:46:10 +0000

    Update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,11 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	if (idx == '\0')
-	{
-		idx = 0;
-	}
-	if (head != NULL)
+	if (*head != NULL)
 	{
 		temp = *head;
 		if (idx == 0)
