d10ecdf93183a87c79f43935c62a7354cc555c19
Lohkrii <joshualopez0429@gmail.com>
2020-11-05 20:16:28 +0000

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,6 +11,10 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int exp = 0, i;
 	int len;
 
+	if (b == '\0')
+	{
+		return (0);
+	}
 	for (len = 0; b[len] != '\0'; len++)
 	{
 		if (b[len] != '1' && b[len] != '0')
