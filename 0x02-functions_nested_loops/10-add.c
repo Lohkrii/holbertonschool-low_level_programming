2305e8f8c14b8dcf5bcc06de071798693ce6b6e1
Lohkrii <joshualopez0429@gmail.com>
2020-11-06 20:51:48 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - Adds 2 ints and returns result.
+ * Return: The result.
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -8,3 +8,4 @@ int _abs(int N);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
+int add(int x, int y);
