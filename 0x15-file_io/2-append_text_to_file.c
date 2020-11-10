4b8a8a237411bc883860e1d5f2b4fb0cac4ffcc2
Lohkrii <joshualopez0429@gmail.com>
2020-11-10 16:31:50 +0000

    Tsk 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,4 +9,27 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	
+	int fd, idx;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (idx = 0; text_content[idx] != '\0'; idx++)
+		{
+		}
+		if (write(fd, text_content, idx) == -1)
+		{
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
