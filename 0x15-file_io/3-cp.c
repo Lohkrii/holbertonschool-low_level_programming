0ad82047d9eaccaecc11384595a41b2da852f6dc
Lohkrii <joshualopez0429@gmail.com>
2020-11-10 18:06:05 +0000

    Update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,34 +9,40 @@
 
 int main(int args, char **str)
 {
-	int from, to, close_first, close_second, read_f;
+	int from_fd, to_fd, close_first, close_second, read_f;
 	char membuf[1024];
 
 	if (args != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	from = open(str[1], O_RDONLY);
-	if (from == -1)
+	from_fd = open(str[1], O_RDONLY);
+
+	if (from_fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read to %s\n", str[1]), exit(98);
 
-	to = open(str[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	if (to == -1)
+	to_fd = open(str[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+
+	if (to_fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str[2]), exit(99);
 
-	while ((read_f = read(from, membuf, 1024)) > 0)
+	while ((read_f = read(from_fd, membuf, 1024)) > 0)
 	{
-		if (write(to, membuf, read_f) == -1)
+		if (write(to_fd, membuf, read_f) == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str[2]), exit(99);
 	}
+
 	if (read_f == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str[1]), exit(98);
 
-	close_first = close(from);
-	if (close_first == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", from), exit(100);
+	close_first = close(from_fd);
 
-	close_second = close(to);
+	if (close_first == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", from_fd), exit(100);
+
+	close_second = close(to_fd);
+
 	if (close_second == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", to), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", to_fd), exit(100);
+
 	return (0);
 }
