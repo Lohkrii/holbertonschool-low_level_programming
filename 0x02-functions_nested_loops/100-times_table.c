d3c735544dbc625fe3b04c944db3269db5e7a559
Lohkrii <joshualopez0429@gmail.com>
2020-11-13 19:56:41 +0000

    Update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,45 +9,44 @@ void print_times_table(int n)
 {
 	int x, y, temp;
 
-	if (n >= 15 || n <= 0)
+	if (n <= 15 && n >= 0)
 	{
-		return;
-	}
-	for (x = 0; x <= n; x++)
-	{
-		for (y = 0; y <= n; y++)
+		for (x = 0; x <= n; x++)
 		{
-			temp = (x * y);
-			if (temp >= 99)
+			for (y = 0; y <= n; y++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(temp / 100 + '0');
-				temp = temp - (temp / 100) * 100;
-				_putchar((temp / 10) + '0');
-				_putchar((temp % 10) + '0');
+				temp = (x * y);
+				if (temp > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(temp / 100 + '0');
+					temp = temp - (temp / 100) * 100;
+					_putchar((temp / 10) + '0');
+					_putchar((temp % 10) + '0');
+				}
+				else if (temp > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((temp / 10) + '0');
+					_putchar((temp % 10) + '0');
+				}
+				else if (y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(temp + '0');
+				}
+				else
+				{
+					_putchar('0');
+				}
 			}
-			else if (temp > 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((temp / 10) + '0');
-				_putchar((temp % 10) + '0');
-			}
-			else if (y != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(temp +'0');
-			}
-			else
-			{
-				_putchar('0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
