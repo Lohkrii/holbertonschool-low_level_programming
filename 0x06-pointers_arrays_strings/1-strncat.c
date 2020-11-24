83c8cd8a037ff3c64b26e3e9c4c3053eccc9cb36
Lohkrii <joshualopez0429@gmail.com>
2020-11-24 21:56:52 +0000

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strncat - Concatonates 2 strings.
+ * @dest: Is the destination we are going to copy to.
+ * @src: Source of what we are copying.
+ * @n: Number of bytes in src.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int idx, cidx;
+
+	for (idx = 0; dest[idx] != '\0'; idx++)
+	;
+
+	for (cidx = 0; *(src + cidx) != '\0' && n > 0; cidx++, n--)
+	{
+		dest[idx + cidx] = src[cidx];
+	}
+	dest[idx + cidx] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -4,4 +4,32 @@
 /* Concatonates 2 strings */
 char *_strcat(char *dest, char *src);
 
+/* Concatonates 2 strings */
+char *_strncat(char *dest, char *src, int n);
+
+/* Copies a string */
+char *_strncpy(char *dest, char *src, int n);
+
+/* Compares 2 strings */
+int _strcmp(char *s1, char *s2);
+
+/* Reverses the content of an array of ints */
+void reverse_array(int *a, int n);
+
+/* Changes lowercase to uppercase */
+char *string_toupper(char *);
+
+/* Capitalizes all words of a string */
+char *cap_string(char *);
+
+/* Ecodes a string into 1337 */
+char *leet(char *);
+
+/* Encodes a string using rot13 */
+char *rot13(char *c);
+
+/* Prints an int */
+void print_number(int n);
+
+
 #endif /* HOLBERTON_H */
