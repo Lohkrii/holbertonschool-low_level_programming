380f10649f18c64f4ca3bdde56a67115c15d2103
Lohkrii <joshualopez0429@gmail.com>
2020-11-25 22:39:30 +0000

    Update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,15 +7,17 @@
  * @new_size: Size of the new pointer.
  * Return: Cantains items of src, plus new size.
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int length, idx;
+	unsigned int length, i;
 	char *n_p, *place_holder;
 
 	place_holder = ptr;
 
 	if (new_size == old_size)
 		return (ptr);
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -25,11 +27,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	n_p = malloc(new_size);
 
 	length = new_size > old_size ? new_size : old_size;
-	for (idx = 0; idx < length; idx++)
+
+	i = 0;
+	while (i < length)
 	{
-		n_p[idx] = place_holder[idx];
+		n_p[i] = place_holder[i];
+		++i;
 	}
-	n_p[idx] = '\0';
+	n_p[i] = '\0';
 	free(ptr);
 	return (n_p);
 }
