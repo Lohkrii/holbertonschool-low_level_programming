d4a54285d1166ad3ba8dfb1e2702cc8bacb06234
Lohkrii <joshualopez0429@gmail.com>
2020-11-25 23:10:13 +0000

    Update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,3 +7,33 @@
  * @separator: String to be printed between the strings.
  * @n: Is the number of arguments passed.
 */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list words;
+	unsigned int idx;
+	char *s;
+
+	va_start(words, n);
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (idx = 0; idx < n; idx++)
+	{
+		s = va_arg(words, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if (idx < (n - 1) && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(words);
+
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,14 @@
 #ifndef variadic_functions
 #define variadic_functions
-/** Sums the passed arguments**/
+/** Sums the passed arguments. **/
 int sum_them_all(const unsigned int n, ...);
 
-/** Prints numbers and separates them **/
-void print_numbers(const char *separato, const unsigned int n, ...);
-/** inputs a singular character **/
+/** Prints numbers and separates them. **/
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+/** Prints strings in arguments separated by format. **/
+void print_strings(const char *separator, const unsigned int n, ...);
+
+/** Inputs a singular character **/
 int _putchar(char x);
 #endif /** variadic_functions **/
