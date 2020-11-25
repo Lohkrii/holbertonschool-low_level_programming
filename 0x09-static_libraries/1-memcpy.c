3a1bf7709a1bec292a1d0b4835c3edac7e2ea383
Lohkrii <joshualopez0429@gmail.com>
2020-11-25 21:00:50 +0000

    Task 0 & 1

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	(void)n;
+	(void)dest;
+	(void)src;
+	return (0);
+}
---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+
+char *_strncat(char *dest, char *src, int n)
+{
+	(void)n;
+	(void)dest;
+	(void)src;
+	return (0);
+}
