c5345da17dca7c6586c6fa8f47741dd19845602a
Lohkrii <joshualopez0429@gmail.com>
2020-11-25 18:19:53 +0000

    Update tsk 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int idx;
 
-	for (idx = 0; idx <= 100; idx++)
+	for (idx = 1; idx <= 100; idx++)
 	{
-		if (idx % 3 == 0)
+		if (idx % 3 == 0 && idx % 5 == 0)
+			printf("FizzBuzz");
+		else if (idx % 3 == 0)
 			printf("Fizz");
 		else if (idx % 5 == 0)
 			printf("Buzz");
-		else if (idx % 3 == 0 && idx % 5 == 0)
-			printf("FizzBuzz");
 		else
 			printf("%d", idx);
 		if (idx < 100)
