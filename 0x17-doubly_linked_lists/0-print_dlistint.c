aac3a54f46c62d41b2ccb7ec64c7503267a20f36
Lohkrii <joshualopez0429@gmail.com>
2020-12-13 20:42:50 +0000

    Added functions in lists.h and finished tsk 1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all nodes in a doubly linked list.
+ * @h: Beggining of list.
+ * Return: Node count.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t idx;
+
+	for(idx = 0; h != NULL; idx++)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+	}
+	return (idx);
+}
