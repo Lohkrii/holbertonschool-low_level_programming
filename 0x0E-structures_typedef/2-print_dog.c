09bc715bba890c97bcc93de1860c809f45123bf0
Lohkrii <joshualopez0429@gmail.com>
2020-12-30 00:26:52 +0000

    Tsk 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
+	}
+	else
+		return;
+}
