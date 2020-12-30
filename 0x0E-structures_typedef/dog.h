b7e8aa6db031a1f2ada750e3c53c93b1bf9d6e5e
Lohkrii <joshualopez0429@gmail.com>
2020-12-30 00:13:17 +0000

    Finished library

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H_
 #define DOG_H_
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
 * struct dog - New data type for a dog
 * @name: First element
@@ -14,5 +17,20 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+
+/* Initializes dog struct */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/* Prints dog */
+void print_dog(struct dog *d);
+
+/* Another struct for dog */
+typedef struct dog dog_t;
+
+/* Creats new dog */
+dog_t *new_dog(char *name, float age, char *owner);
+
+/* Frees dog memory*/
+void free_dog(dog_t*d);
 
 #endif /* DOG_H_ */
