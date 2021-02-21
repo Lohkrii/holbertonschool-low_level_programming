291c8632579050b96652b0bdd3393ca650e4a3da
Lohkrii <joshualopez0429@gmail.com>
2021-02-21 20:43:28 +0000

    5-island_perimeter.py: Fixed typos in code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,6 +3,7 @@
     Calculates the perimeter of an island in matrix form.
 """
 
+
 def def island_perimeter(grid):
     """ Calculates the perimeter of 1's in a grid """
     count = 0
@@ -10,8 +11,8 @@ def def island_perimeter(grid):
         for jidx in range(len(grid[idx])):
             if grid[idx][jidx] == 1:
                 count += 4
-                if idx > 0 and grid[idx -1][jidx] == 1:
-                    count -= 
+                if idx > 0 and grid[idx - 1][jidx] == 1:
+                    count -= 2
                 if jidx > 0 and grid[idx][jidx - 1] == 1:
                     count -= 2
     return count
