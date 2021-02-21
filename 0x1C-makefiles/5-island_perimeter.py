2c59b4e10d4ced7b85be303834c4ed78eb34f123
Lohkrii <joshualopez0429@gmail.com>
2021-02-21 20:39:57 +0000

    Adding task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Calculates the perimeter of an island in matrix form.
+"""
+
+def def island_perimeter(grid):
+    """ """
+    count = 0
+    for idx in range(len(grid)):
+        for jidx in range(len(grid[idx])):
+            if grid[idx][jidx] == 1:
+                count += 4
+                if idx > 0 and grid[idx -1][jidx] == 1:
+                    count -= 
+                if jidx > 0 and grid[idx][jidx - 1] == 1:
+                    count -= 2
+    return count
